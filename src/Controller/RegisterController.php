d949669809937ffcaaa4bef96b1a4c19037e2b15
TOD <ion.todorov1987@gmail.com>
2024-11-19 06:52:38 +0100

    reglee la probleme avec importation de paquets pour notre formulaire

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterUserType;
+//use App\Controller\RegisterUserType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
     public function index(): Response
     {
-        return $this->render('register/index.html.twig');
+        
+        //$user = new User();
+        //$form = $this->createForm(RegisterUserType::class,$user);
+
+        $form = $this -> createForm(RegisterUserType::class);
+        
+        return $this -> render('register/index.html.twig',[
+            'registerForm' => $form->createView()
+        ]);
     }
 }
+
+
+// USE -> je appelle un repertoire
+// NAMESPACE -> Je definis un repertoire
