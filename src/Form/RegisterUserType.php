6fadf9e9f39082f8d0961b353a8930f3429bf6dd
TOD <ion.todorov1987@gmail.com>
2024-11-21 05:48:10 +0100

    modifier notre formulaire

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,12 +16,38 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class,[
+                'label' => "Votre adresse e-mail",
+                'attr' => [
+                    'placeholder' => "Indiquez votre boite mail"
+                ]
+            ])
             //->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('submit',SubmitType::class);
+            ->add('password',PasswordType::class,[
+                'label' => "Votre mot de passe",
+                'attr' => [
+                    'placeholder' => "Password cache"
+                ]
+            ])
+            ->add('firstname',TextType::class,[
+                'label' => "Votre prenom",
+                'attr' => [
+                    'placeholder' => "Prenom"
+
+                ]
+            ])
+            ->add('lastname',TextType::class,[
+                'label' => "Nom",
+                'attr' => [
+                    'placeholder' => "Nom"
+                ]
+            ])
+            ->add('submit',SubmitType::class,[
+                'label' => "Valider",
+                'attr' => [
+                    'class' => "btn btn-success" // a chercher sur bootstrap la couleur de button
+                ]
+            ]);
         ;
     }
 
