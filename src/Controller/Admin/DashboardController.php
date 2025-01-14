75c5aac3945f7d8a814c89cdecfd3e4aae432e70
TOD <ion.todorov1987@gmail.com>
2025-01-14 06:20:57 +0100

    2 nouveau entité

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -44,5 +46,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Categorie', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-list', Product::class);
     }
 }
---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+// use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\SlugType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Produit')
+            ->setEntityLabelInPlural('Produits')
+            //->setDateFormat('...')
+            // ...
+        ;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')->setLabel('Nom')->setHelp('Nom de votre produit'),
+            SlugField::new('slug')->setTargetFieldName('name')->setLabel('URL')->setHelp('URL de votre produit generee automatiquement'),
+            TextEditorField::new('description')->setLabel('Description')->setHelp('Description de votre produit'),
+            ImageField::new('Illustration')->setLabel('Image')->setUploadDir('/public/uploads')->setHelp('Image du votre produit 600x600'),
+            NumberField::new('price')->setLabel('Prix H.T.')->setHelp('Pris HT du produit sans sigle'),
+            ChoiceField::new('tva')->setLabel('Taux de TVA')->setChoices([
+                '5,5' => '5,5',
+                '10' => '10',
+                '20' => '20',
+            ]),
+            AssociationField::new('category','Categorie associée'),// creation de fonction dans entité category __tostring
+        ];
+    }
+    
+}
