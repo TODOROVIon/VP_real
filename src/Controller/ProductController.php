ec69ace499903401fd5286cc19d0ced4e184a520
TOD <ion.todorov1987@gmail.com>
2025-02-24 17:25:12 +0100

    apprendre auto-mapping

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,6 +10,20 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProductController extends AbstractController
 {
     #[Route('/produit/{slug}', name: 'app_product')]
+
+    /*public function index(#[MapEntity(slug: 'slug')] Product $product): Response      une maniere AutoMaping existante pour gagné dans la ligne de code       //#[MapEntity(slug: 'slug') -> annotation
+    {
+
+        if(!$product){
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('product/index.html.twig', [
+            'product' => $product,
+        ]);
+    }
+    */
+
     public function index($slug,ProductRepository $productRepository ): Response
     {
         $product = $productRepository->findOneBySlug($slug);
