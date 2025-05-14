294ce12f34ee548b683f7220cf8b57a867bdd036
TOD <ion.todorov1987@gmail.com>
2025-05-14 14:56:44 +0200

    Tunnel d'achat. Recapitulatif de notre panier avec l'adresse et livreur choisie

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 final class OrderController extends AbstractController
 {
@@ -17,16 +19,50 @@ final class OrderController extends AbstractController
     #[Route('/commande/livraison', name: 'app_order')]
     public function index(): Response
     {
-        $addresses =$this->getUser()->getAddresses();
+        $addresses = $this->getUser()->getAddresses();
         if (count($addresses)==0){
             return $this->redirectToRoute('app_account_address_form');
         }
         $form = $this->createForm(OrderType::class, null, [
-            
+            'addresses' => $addresses,
+            'action' => $this->generateUrl('app_order_summary'),
         ]);
 
         return $this->render('order/index.html.twig', [
             'deliveryForm' => $form->createView(),
         ]);
     }
+
+    /*
+        2eme etape du tunnel d'achat
+            Recapitulatif de la commande de l'utilisateur
+            Insertion dans la BDD
+            Preparation du payement vers Stripe
+    */
+    // methods: ['POST']
+    #[Route('/commande/recapitulatif', name: 'app_order_summary' )]
+    public function add(Request $request, Cart $cart): Response
+    {
+        if ($request->getMethod() != 'POST'){
+            return $this->redirectToRoute('app_cart');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'addresses' => $this->getUser()->getAddresses(),
+        ]);
+
+        $form ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            // dd($form->getData());
+            // Stocker les information dans BDD
+        }
+
+        return $this->render('order/summary.html.twig',[
+            'choises' => $form->getData(),
+            'cart' => $cart->getCart(),
+            'totalWt' => $cart->getTotalWt(),
+        ]);
+    }
+
 }
